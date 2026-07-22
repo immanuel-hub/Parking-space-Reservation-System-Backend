9d95ac60d419a0d97a3958a7cca6d9ab80ccc42b
immanuel <immanuel.okoth@student.moringaschool.com>
2026-07-22 12:32:56 +0300

    added a class user section which stores both drivers and admins

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = [('driver', 'Driver'), ('admin', 'Admin')]
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='driver')
+    phone_number = models.CharField(max_length=15, blank=True)
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
--- parking_system/settings.py
+++ parking_system/settings.py
@@ -37,9 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'corsheaders',
+
+    'api',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -115,3 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+CORS_ALLOW_ALL_ORIGINS = True
+AUTH_USER_MODEL = 'api.User'
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
