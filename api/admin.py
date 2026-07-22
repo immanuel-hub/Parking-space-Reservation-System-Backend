1f8d678acff495282061d4a55190049d913e8dfb
immanuel <immanuel.okoth@student.moringaschool.com>
2026-07-22 15:29:48 +0300

    added admin dashboard access for managing parking data in admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, ParkingLot, ParkingSpace, Reservation, Vehicle
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(ParkingLot)
+admin.site.register(ParkingSpace)
+admin.site.register(Reservation)
+admin.site.register(Vehicle)
