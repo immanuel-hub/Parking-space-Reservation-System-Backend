b50414ba32a84a6d9dcbbad527ca74d4d532b519
immanuel <immanuel.okoth@student.moringaschool.com>
2026-07-22 12:38:13 +0300

    added a parking lot management feature that contain multiple parking spaces

--- api/models.py
+++ api/models.py
@@ -8,3 +8,12 @@ class User(AbstractUser):
 
     def __str__(self):
         return f"{self.username} ({self.role})"
+
+class ParkingLot(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=255)
+    capacity = models.PositiveIntegerField(default=10)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.location} "
