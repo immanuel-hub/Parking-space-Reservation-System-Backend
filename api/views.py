acc878cb23f2874665f18795b5b1c3ef3c8d31ba
immanuel <immanuel.okoth@student.moringaschool.com>
2026-07-22 15:16:15 +0300

    user registration endpoint, parking lot management

--- api/views.py
+++ api/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions, generics
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-# Create your views here.
+from .models import ParkingLot, ParkingSpace, Reservation, Vehicle
+from .serializers import (
+    UserSerializer,
+    RegisterSerializer,
+    ParkingLotSerializer,
+    ParkingSpaceSerializer,
+    ReservationSerializer,
+    VehicleSerializer
+)
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+class ParkingLotViewSet(viewsets.ModelViewSet):
+    queryset = ParkingLot.objects.all()
+    serializer_class = ParkingLotSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [permissions.IsAdminUser()]
+        return super().get_permissions()
